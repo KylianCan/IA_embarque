94bfd9d59157ab919d068ef0ad2118398020eafe
KylianCan <165375095+KylianCan@users.noreply.github.com>
2025-02-01 23:53:14 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ RUN docker build -t student .
 RUN cp -r inference/ Work/
 RUN ./student.sh
 RUN cd Work/inference/
-RUN python3 main.py
+RUN python3 MLP_.py
 RUN gcc -W -o inference main.c Bmp2Matrix.c Bmp2Matrix.h -lm
 
